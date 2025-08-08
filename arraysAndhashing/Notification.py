200153746131d38363bebfccd926b224d6114c5e
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2025-08-07 23:03:20 -0600

    fix

--- arraysAndhashing/Notification.py
+++ arraysAndhashing/Notification.py
@@ -1,4 +1,33 @@
 def prepareNotification(message, k):
+    k -= 3
+    temp = ""
+    total = 0
+    messages = message.split(" ")
+    if len(messages[0]) > k:
+        return " "
+    if len(message) <= k:
+        return message
+    for i in message:
+        if i.isalpha():
+            temp += i
+        if i == " ":
+            if total + 1 + len(temp) > k:
+                continue  
+            total = total + 1 + len(temp)
+            temp = ""
+    return message[:total-1] + "..."
+
+print(prepareNotification( "And now here is my secret",15))
+
+print(prepareNotification( "There is an animal with four legs",15))
+
+print(prepareNotification( "super dog",4))
+
+print(prepareNotification( "how are you",20))
+
+
+# Echo con IA
+def prepareNotification(message, k):
     # Espacio disponible antes de los puntos suspensivos
     available = k - 3
     if len(message) <= k:
