96c68638473cb84816f0b2b441d37849e7214527
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2025-08-07 22:02:35 -0600

    problems

---
+++ arraysAndhashing/diagonal.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    right = 0 
+    left = 0
+    n = len(arr)
+    final = n-1
+    for i in range(n):
+        right += arr[i][i]
+        left += arr[i][final]
+        print(right, left)
+        final -= 1
+    return abs(right -left )
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
